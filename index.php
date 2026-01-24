6197e0c4ac7310d59786f0a14f4dff984ecb965f
DevMahbubHussain <devmahbubmamun@gmail.com>
2026-01-24 18:35:47 +0600

    Data Type Update and return type update

--- index.php
+++ index.php
@@ -14,9 +14,9 @@ $student1->email = 'devmahbubmamun@gmail.com';
 $student1->address = 'Dhaka';
 
 $result1 = new Result();
-$result1->physics = '85';
-$result1->math = '90';
-$result1->chemistry = '88';
+$result1->physics = 85;
+$result1->math = 90;
+$result1->chemistry = 88;
 $result1->GetAverage();
 $result1->GetPassOrFail();
 
@@ -29,9 +29,9 @@ $student2->email = 'mahbubhussaincse@gmail.com';
 $student2->address = 'Cumilla';
 
 $result2 = new Result();
-$result2->physics = '85';
-$result2->math = '90';
-$result2->chemistry = '88';
+$result2->physics = 85;
+$result2->math = 90;
+$result2->chemistry = 88;
 
 $result1->GetAverage();
 $result1->GetPassOrFail();
